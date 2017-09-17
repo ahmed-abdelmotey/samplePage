f895a05042dfb6a9a9d19af4884dde4d0fb0804d
Ahmed Abdelmotey <ahmed.abdelmotey92@gmail.com>
2017-09-17 03:16:26 -0700

    done

---
+++ js/logme.js
@@ -0,0 +1,10 @@
+// clear()
+console.log('%c ahmed mohamed abde elmotey', 'font-size:1.5em');
+console.log('%c javascript full stack developer', 'font-size:1em; color:#333');
+console.log('%c mobile: 01000 462 340', 'font-size:1em; color:#333');
+console.log('%c email: ahmed.abdelmotey92@gmail.com', 'font-size:1em; color:#333');
+console.log('%c check me on', 'font-size:1em; color:#999');
+console.log('%c LinkedIn: https://www.linkedin.com/in/ahmedabdelmotey/', 'font-size:1em; color:#666');
+console.log('%c GitHub: https://github.com/ahmed-abdelmotey', 'font-size:1em; color:#666');
+console.log('%c Behance: https://www.behance.net/ahmedabdel42d0', 'font-size:1em; color:#666');
+console.log('%c ........................................................', 'font-size:1em; color:#666');
